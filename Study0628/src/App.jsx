05b48bb8ba5d9bedc3a417743513d68ade138d09
윤성연 <s2427@e-mirim.hs.kr>
2025-07-19 20:10:09 +0900

    0628

--- Study0628/src/App.jsx
+++ Study0628/src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
 import Intro from './components/Intro'
+import Post from './components/Post'
+import PostList from './components/PostList'
+import Timer from './components/Timer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isClick, setClick] = useState(false);
 
   return (
     <>
-      <Intro />
+      <div style={{display: "flex", gap: "60px", margin: "40px"}}>
+        {/* <Intro /> */}
+
+        <Post />
+        
+        <div style={{width: "400px", paddingTop: "50px"}}>
+          <button onClick={() => setClick(!isClick)}>불러오기</button>
+          {isClick ? <PostList /> : null}
+        </div>
+
+        <Timer />
+      </div>
     </>
   )
 }
